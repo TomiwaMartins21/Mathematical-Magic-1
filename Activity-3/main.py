105fa5213a07cfee620444728daebd56ad336784
TomiwaMartins21 <martinstomiwa838@gmail.com>
2025-04-26 13:53:20 +0100

    Initial commit

---
+++ Activity-3/main.py
@@ -0,0 +1,5 @@
+# Program to convert roman numerals to integers 
+def romanToInt(romanInput):
+
+    # All roman units with integer equivalent values
+    roman = ('M'1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1)
